7dc1f44772e667ddaea284b0a64e35a735cfb032
hayfa safa <haifasafaa@gmail.com>
2025-12-01 11:39:09 +0100

    add html file

---
+++ Cart-shop.js
@@ -0,0 +1,17 @@
+let iconButton = document.getElementById('icon-button'); 
+let addItems = document.getElementById('add-items'); 
+let removeItems = document.getElementById('remove-items')
+
+ let quantity = 0;  
+
+function addIconToCart() {
+
+}
+
+ function updateCart () {
+  removeItemsFromCart (
+    
+  )
+
+ }
+ 
