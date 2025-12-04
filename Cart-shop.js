3832673c4c083fbe0d50576da01c088c33c12313
hayfa safa <haifasafaa@gmail.com>
2025-12-04 21:02:46 +0100

    create array

---
+++ Cart-shop.js
@@ -0,0 +1,30 @@
+let order ument.getElementById('shop');
+
+let 
+
+
+
+
+
+
+let generateShop =()=>{
+return `
+ (shop.innerHTML =
+  <div class="item">
+    <img src="A_small_cup_of_coffee.jpeg" alt="cafe late">
+    <div class="details">
+      <h3>cafe late</h3>
+      <div class="price-quantity">
+        <h2>$ 45</h2>
+          <div class="buttons">
+      <i class="fa-sharp-duotone fa-regular fa-minus"></i>
+      <div class="quantity">0</div>
+      <i class="fa-sharp-duotone fa-regular fa-plus"></i>
+    </div>
+      </div>
+    </div>
+  </div>
+`
+}
+
+generateShop(); 
