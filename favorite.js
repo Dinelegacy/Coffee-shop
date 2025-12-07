981c8bc36bfec7551fea0fef3b0e29829e37bf01
Jalal-Dine Karim <dinelegacy@gmail.com>
2025-12-07 23:54:19 +0100

    add js logic in favorite.js

--- favorite.js
+++ favorite.js
@@ -3,7 +3,16 @@ const favButtons = document.querySelectorAll('.favorite-btn');
 
 favButtons.forEach(button => {
     button.addEventListener('click', (event)=>{
-        
 
+        const itemId = event.target.dataset.id;
+        favorites.push(itemId);
+        console.log(favorites);
+
+      
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+      const savedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
     })
 })
+
+
+
