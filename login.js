aa3bae7b838dd86383a28c4b5bf5cfc8e3bd47ef
sandy <sandyezzat70@gmail.com>
2025-12-09 15:20:27 +0100

    add the humburhe menu to the login page

---
+++ login.js
@@ -0,0 +1,25 @@
+// Hamburger Menu
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+}); 
+
+// Button Table
+document.querySelector("#buttonTable").onclick = (event) => {
+    window.location.href = "menu.html";
+};
+
+// Button Menu
+document.querySelector("#buttonMenu").onclick = (event) => {
+    window.location.href = "menu.html";
+};
+
+
+// Button TakeAway
+document.querySelector("#buttonTakeAway").onclick = (event) => {
+    window.location.href = "menu.html";
+};
