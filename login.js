81d1e6e19c1372b648437642f54ef78aac00cfcf
sandy <sandyezzat70@gmail.com>
2025-12-10 17:59:56 +0100

    add js file to login page

--- login.js
+++ login.js
@@ -1,17 +1,39 @@
-const form = document.getElementById("form");
+const form = document.getElementById("form-inputs");
 const emailInput = document.getElementById("email-input");
 const passwordInput = document.getElementById("password-input");
 const errorMessage = document.getElementById("error-message");
+const submitButton = document.getElementById(".login");
 
 form.addEventListener("submit", (e) => {
   let error = [];
 
-  error = getLoginPageErrors(emailInput, passwordInput.value);
-  if (error.lenght > 0) {
+  error = getLoginPageErrors(emailInput.value, passwordInput.value);
+  if (error.length > 0) {
     e.preventDefault();
     errorMessage.innerText = error.join(", ");
   }
 });
 
-function getLoginPageErrors() {}
+function getLoginPageErrors(email, password) {
+  let errors = [];
 
+  if (email === "" || email == null) {
+    errors.push("Email is required");
+    emailInput.parentElement.classList.add("incorrect");
+  }
+
+  if (password === "" || password == null) {
+    errors.push("Password is required");
+    passwordInput.parentElement.classList.add("incorrect");
+  }
+
+  return errors;
+}
+
+const allInputs = [emailInput, passwordInput];
+allInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    input.parentElement.classList.remove("incorrect");
+    errorMessage.innerText = "";
+  });
+});
