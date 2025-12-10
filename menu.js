1fdecf6f75f1fdc2af910a384287db938027f666
hayfa safa <haifasafaa@gmail.com>
2025-12-10 13:04:37 +0100

    the pull succefull

--- menu.js
+++ menu.js
@@ -28,10 +28,6 @@ function setTopBackground(type) {
 const menuData = {
   promotions: {
     items: [
-      { img: "assets/menu/Gingerbread Milkshake Mocktail.jpg",
-         name: "Gingerbread Milkshake Mocktail", 
-         price: 45
-        },
       { img: "assets/menu/Sandman Latte with gräde.jpg", 
         name: "Sandman latte with Gräde", 
         price: 45
@@ -88,46 +84,46 @@ const menuData = {
   },
   tea: {
     items: [
-      { img: "assets/menu/Orange Spiced Black Tea.jpg", name: "Orange Spiced Black Tea", price: "28:-" },
-      { img: "assets/menu/Tea with lemon.jpg", name: "Tea with lemon", price: "30:-"},
-      { img: "assets/menu/Green Tea.jpg", name: "Green Tea", price: "32:-"}
+      { img: "assets/menu/Orange Spiced Black Tea.jpg", name: "Orange Spiced Black Tea", price: 28 },
+      { img: "assets/menu/Tea with lemon.jpg", name: "Tea with lemon", price: 30},
+      { img: "assets/menu/Green Tea.jpg", name: "Green Tea", price: 32}
     ]
   },
   smoothies: {
     items: [
-      { img: "assets/menu/Iced Matcha Latte.jpg", name: "Iced Matcha Latte", price: "55:-" },
-      { img: "assets/menu/Iced Brown Sugar Matcha Latte.jpg", name: "Iced Brown Sugar Matcha Latte", price: "58:-"},
-      { img: "assets/menu/Velvety Chocolate Hazelnut Milkshake.jpg", name: "Velvety Chocolate Hazelnut Milkshake", price: "52:-"},
-      { img: "assets/menu/Peanut Butter Frappuccino.jpg", name: "Peanut Butter Frappuccino", price: "55:-"},
-      { img: "assets/menu/Rich Latte.jpg", name: "Rich Latte", price: "58:-"},
-      { img: "assets/menu/Caramel Iced Coffee.jpg", name: "Caramel Iced Coffee", price: "52:-"},
-      { img: "assets/menu/Lincoln Mint Mocha.jpg", name: "Lincoln Mint Mocha", price: "55:-"},
-      { img: "assets/menu/Black Gold Iced Coffee.jpg", name: "Black Gold Iced Coffee", price: "58:-"},
-      { img: "assets/menu/Chocolate Chip Frappuccino.jpg", name: "Chocolate Chip Frappuccino", price: "52:-"},
-      { img: "assets/menu/Iced Coffee Mojito.jpg", name: "Iced Coffee Mojito", price: "55:-"},
-      { img: "assets/menu/Mojito Espresso.jpg", name: "Mojito Espresso", price: "58:-"},
-      { img: "assets/menu/Coffee Mojito.jpg", name: "Coffee Mojito", price: "52:-"},
-      { img: "assets/menu/MANGO MILKSHAKE.jpg", name: "Mango Milkshake", price: "55:-"},
-      { img: "assets/menu/Whipped Dalgona Coffee.jpg", name: "Whipped Dalgona Coffee", price: "58:-"}
+      { img: "assets/menu/Iced Matcha Latte.jpg", name: "Iced Matcha Latte", price: 55 },
+      { img: "assets/menu/Iced Brown Sugar Matcha Latte.jpg", name: "Iced Brown Sugar Matcha Latte", price: 58},
+      { img: "assets/menu/Velvety Chocolate Hazelnut Milkshake.jpg", name: "Velvety Chocolate Hazelnut Milkshake", price: 52},
+      { img: "assets/menu/Peanut Butter Frappuccino.jpg", name: "Peanut Butter Frappuccino", price: 55},
+      { img: "assets/menu/Rich Latte.jpg", name: "Rich Latte", price: 58},
+      { img: "assets/menu/Caramel Iced Coffee.jpg", name: "Caramel Iced Coffee", price: 52},
+      { img: "assets/menu/Lincoln Mint Mocha.jpg", name: "Lincoln Mint Mocha", price: 55},
+      { img: "assets/menu/Black Gold Iced Coffee.jpg", name: "Black Gold Iced Coffee", price: 58},
+      { img: "assets/menu/Chocolate Chip Frappuccino.jpg", name: "Chocolate Chip Frappuccino", price: 52},
+      { img: "assets/menu/Iced Coffee Mojito.jpg", name: "Iced Coffee Mojito", price: 55},
+      { img: "assets/menu/Mojito Espresso.jpg", name: "Mojito Espresso", price: 58},
+      { img: "assets/menu/Coffee Mojito.jpg", name: "Coffee Mojito", price: 52},
+      { img: "assets/menu/MANGO MILKSHAKE.jpg", name: "Mango Milkshake", price: 55},
+      { img: "assets/menu/Whipped Dalgona Coffee.jpg", name: "Whipped Dalgona Coffee", price: 58}
     ]
   },
   snacks: {
     items: [
-      { img: "assets/menu/Lemon Cheesecake.jpg", name: "Lemon Cheesecake", price: "25:-" },
-      { img: "assets/menu/Espresso Chocolate Cheesecake.jpg", name: "Espresso Chocolate Cheesecake", price: "22:-"},
-      { img: "assets/menu/Creamy Cheesecake with strawberry.jpg", name: "Creamy Cheesecake with strawberry", price: "30:-"},
-      { img: "assets/menu/Chocolate croissant.jpg", name: "Chocolate Croissant", price: "25:-"},
-      { img: "assets/menu/croissant sandwich.jpg", name: "Croissant Sandwich", price: "22:-"},
-      { img: "assets/menu/Ham Cheese And Lettuce Sandwich.jpg", name: "Ham Cheese And Lettuce Sandwich", price: "30:-"},
-      { img: "assets/menu/Pistachio Croissant.jpg", name: "Pistachio Croissant", price: "25:-"},
-      { img: "assets/menu/Coconut matcha cream pie.jpg", name: "Coconut matcha cream pie", price: "22:-"},
-      { img: "assets/menu/chocolate donat.jpg", name: "Chocolate Donut", price: "30:-"},
-      { img: "assets/menu/Spiced Apple Cider Donuts with Caramel Glaze.jpg", name: "Spiced Cider Donuts with Caramel Glaze", price: "25:-"},
-      { img: "assets/menu/Red Velvet Strawberry Cheesecake.jpg", name: "Red Velvet Strawberry Cheesecake", price: "22:-"},
-      { img: "assets/menu/Black Forest Cake Roll.jpg", name: "Black Forest Cake Roll", price: "30:-"},
-      { img: "assets/menu/tiramisu mousse cake.jpg", name: "tiramisu mousse cake", price: "25:-"},
-      { img: "assets/menu/Dark Chocolate Bars with Pistachio Paste Filling.jpg", name: "Dark Chocolate Bars with Pistachio", price: "22:-"},
-      { img: "assets/menu/Chocolate Cake with Strawberries.jpg", name: "Chocolate Cake with Strawberries", price: "30:-"}
+      { img: "assets/menu/Lemon Cheesecake.jpg", name: "Lemon Cheesecake", price: 25 },
+      { img: "assets/menu/Espresso Chocolate Cheesecake.jpg", name: "Espresso Chocolate Cheesecake", price: 22},
+      { img: "assets/menu/Creamy Cheesecake with strawberry.jpg", name: "Creamy Cheesecake with strawberry", price: 30},
+      { img: "assets/menu/Chocolate croissant.jpg", name: "Chocolate Croissant", price: 25},
+      { img: "assets/menu/croissant sandwich.jpg", name: "Croissant Sandwich", price: 22},
+      { img: "assets/menu/Ham Cheese And Lettuce Sandwich.jpg", name: "Ham Cheese And Lettuce Sandwich", price: 30},
+      { img: "assets/menu/Pistachio Croissant.jpg", name: "Pistachio Croissant", price: 25},
+      { img: "assets/menu/Coconut matcha cream pie.jpg", name: "Coconut matcha cream pie", price: 22},
+      { img: "assets/menu/chocolate donat.jpg", name: "Chocolate Donut", price: 30},
+      { img: "assets/menu/Spiced Apple Cider Donuts with Caramel Glaze.jpg", name: "Spiced Cider Donuts with Caramel Glaze", price: 25},
+      { img: "assets/menu/Red Velvet Strawberry Cheesecake.jpg", name: "Red Velvet Strawberry Cheesecake", price: 22},
+      { img: "assets/menu/Black Forest Cake Roll.jpg", name: "Black Forest Cake Roll", price: 30},
+      { img: "assets/menu/tiramisu mousse cake.jpg", name: "tiramisu mousse cake", price: 25},
+      { img: "assets/menu/Dark Chocolate Bars with Pistachio Paste Filling.jpg", name: "Dark Chocolate Bars with Pistachio", price: 22},
+      { img: "assets/menu/Chocolate Cake with Strawberries.jpg", name: "Chocolate Cake with Strawberries", price: 30}
     ]
   }
 };
