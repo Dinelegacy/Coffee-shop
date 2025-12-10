acfab8dbed4776c0210d97344d59dc8c199431ab
hayfa safa <haifasafaa@gmail.com>
2025-12-10 12:06:04 +0100

    commit before pull

--- favorite.js
+++
@@ -1,9 +0,0 @@
-const favorites = [];
-const favButtons = document.querySelectorAll('.favorite-btn');
-
-favButtons.forEach(button => {
-    button.addEventListener('click', (event)=>{
-        
-
-    })
-})
