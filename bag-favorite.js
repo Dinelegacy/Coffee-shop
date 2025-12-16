a46ba94b1e0aa5f7452067f44a3258f8942d80b2
hayfa safa <haifasafaa@gmail.com>
2025-12-17 00:41:42 +0100

    fix favorite bag

--- bag-favorite.js
+++ bag-favorite.js
@@ -1,11 +1,11 @@
-// favorite-cart.js
-import { addItemToCart , updateBagCount } from "./cart-handler.js";
+ // bag-favorite.js
+import { addItemToCart, updateBagCount } from "./cart-handler.js";
 
- 
 document.addEventListener("DOMContentLoaded", () => {
-  
+ 
   updateBagCount();
 
+  
   const bagIcon = document.getElementById("bagIcon");
   if (bagIcon) {
     bagIcon.style.cursor = "pointer";
@@ -14,31 +14,41 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+ 
   const favoriteContainer = document.getElementById("favorite-container");
   if (!favoriteContainer) return;
 
-  favoriteContainer.addEventListener("click", (buttonFavorite) => {
-    const btn = buttonFavorite.target.closest(".add-favorite");
+  favoriteContainer.addEventListener("click", (favoriteButton) => {
+    const btn = favoriteButton.target.closest(".add-favorite");
     if (!btn) return;
 
     const item = btn.closest(".favorite-item");
     if (!item) return;
 
-    const index = item.dataset.index;
+    const index = Number(item.dataset.index);
     const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
     const favItem = favorites[index];
     if (!favItem) return;
 
-     const id = favItem.name.trim().toLowerCase().replace(/\s+/g, "-");
+     
+    const id = favItem.name.trim().toLowerCase().replace(/\s+/g, "-");
 
+     
+    const cleanPrice = Number(
+      String(favItem.price).replace(/[^\d.]/g, "")
+    );
+
+    if (isNaN(cleanPrice)) {
+      console.error("Invalid price:", favItem.price);
+      return;
+    }
 
     addItemToCart(
       id,
       favItem.name,
-      parseFloat(favItem.price),
+      cleanPrice,
       favItem.image,
       1
     );
   });
 });
-
