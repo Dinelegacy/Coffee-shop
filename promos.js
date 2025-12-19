0c01c5dae2cc1d0c519c53d9aaabd495f12b3253
Yuri <yuri.cabrera.riv@gmail.com>
2025-12-19 11:26:33 +0100

    fix: adjust navbar height and section positioning for better layout

--- promos.js
+++ promos.js
@@ -2,4 +2,12 @@
     window.location.href = "menu.html";
 };
 
- 
+// Hamburger Menu
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener('click', () => {
+    // Toggle hamburger menu
+    const isActive = hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+}
